26b56e76399de09605a0a4ee7ab421f7dec5a1be
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-26 22:52:06 -0500

    Update and rename Repositorio/C++/tipos de entradas.cpp to  Repositorio/ C ++/tipos de entradas.cpp

---
+++ Repositorio/ C ++/tipos de entradas.cpp
@@ -0,0 +1,23 @@
+//Leer una cadena de texto con espacios
+
+string cadena;
+getline(cin,cadena);
+
+//N casos de prueba
+
+int n;
+while(n--){
+	//Instrucciones
+}
+
+//Casos de prueba hasta EOF
+
+while(cin>>n){
+	//Instrucciones
+}
+
+//EOF con scanf
+
+while(scanf("%d", &n)!=EOF){
+	//Instrucciones
+} 
