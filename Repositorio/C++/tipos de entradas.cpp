6c47bbd2ec8095c5ba4a55997d78aad736143006
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-26 22:12:55 -0500

    Update tipos de entradas.cpp

--- Repositorio/C++/tipos de entradas.cpp
+++ Repositorio/C++/tipos de entradas.cpp
@@ -1,1 +1,20 @@
-//:0 
+//Entradas por N casos
+
+int n;
+cin>>n;
+while(n--){
+  
+} 
+
+//Entrada hasta EOF
+
+while(cin>>n){
+	
+	//Instrucciones
+} 
+//Entrada hasta EOF con scanf
+
+while(scanf("%d",&n)!=EOF){
+  //Instrucciones
+} 
+
