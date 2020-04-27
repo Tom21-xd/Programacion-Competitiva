de846eadd919d85d8de2c257a00c5e69b89e3870
codeBreakerUA <64387188+codeBreakerUA@users.noreply.github.com>
2020-04-26 21:28:49 -0500

    Repositorio

---
+++ Repositorio/Python/tipo de entradas.py
@@ -0,0 +1,16 @@
+#Leer varios datos en una sola linea
+
+a = input().split(' ')
+
+#N casos
+
+while n:
+	n-=1
+	#Instrucciones
+
+# EOF	
+
+while True:
+	try: #Instrucciones
+	except EOFError:
+		break
