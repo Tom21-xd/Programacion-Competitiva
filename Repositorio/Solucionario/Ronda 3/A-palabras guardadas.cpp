b60fa4dc835bf0f6a7f8c4677ebf049b528afe45
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-28 17:57:10 -0500

    Update A-palabras guardadas.cpp

--- Repositorio/Solucionario/Ronda 3/A-palabras guardadas.cpp
+++ Repositorio/Solucionario/Ronda 3/A-palabras guardadas.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
-int main(){
-	fast;
+int main()
+{
+	set<string> guardar;
+	string str;
+	int n, nP;
+	cin>>n;
+	while(n--){
+		guardar.clear(); //Se vacia
+		cin>>nP;
+		while(nP--){
+			cin>>str;
+			//Pasa la cadena a mayúsculas
+			transform(str.begin(),str.end(),str.begin(),::toupper);
+			guardar.insert(str);
+		}
+		//Se muestra en orden inverso
+		for(auto i=guardar.rbegin(); i!=guardar.rend(); i++)
+			cout<<*i<<"\n";
+	}
 	return 0;
 }
