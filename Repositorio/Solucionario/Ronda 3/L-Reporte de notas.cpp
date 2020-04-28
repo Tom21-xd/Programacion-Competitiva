62f0fd6a7fe2e01c40388e0203be5e63a2f173c7
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-28 18:47:58 -0500

    Update L-Reporte de notas.cpp

--- Repositorio/Solucionario/Ronda 3/L-Reporte de notas.cpp
+++ Repositorio/Solucionario/Ronda 3/L-Reporte de notas.cpp
@@ -1,7 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
-int main(){
-	fast;
+int main()
+{
+	int n, f, id;
+	double note;
+	map<int,pair<string,double>> inp;
+	string str;
+	cin>>n;
+	cout<<fixed<<setprecision(1); //Para aproximar a un decimal la nota
+	while(n--){
+		inp.clear(); //Se vacía 
+			cin>>f;
+			for(int i=0; i<f; i++){ //Se lee nombres e id
+				cin>>str>>id;
+				inp[id].first=str; //El dato first es el nombre
+			}
+			while(f--){ //Se lee el id y las notas
+				cin>>id>>note;
+				inp[id].second=note; //El dato second es la nota
+			}
+			//Se muestra los datos de manera nombre y nota
+			for(auto it : inp) //El it toma el valor de la llave
+				cout<<it.second.first<<" "<<it.second.second<<"\n";
+	}
 	return 0;
 }
