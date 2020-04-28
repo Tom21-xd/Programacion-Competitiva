b344d92bb0e4470a3621ac709c33cc37d5e0e737
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-28 18:36:01 -0500

    Update K-Carrito de compras.cpp

--- Repositorio/Solucionario/Ronda 3/K-Carrito de compras.cpp
+++ Repositorio/Solucionario/Ronda 3/K-Carrito de compras.cpp
@@ -1,7 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
-int main(){
-	fast;
+
+int main()
+{
+	int n, f, c, prec, total;
+	vector<string> inp, out;
+	unordered_map<string, int> dic; //Como no hay necesidad de ordenar, uso esta estructura
+	string str;
+	cin>>n;
+	while(n--){
+		total=0;
+		cin>>f;
+		while(f--){ //Lista de productos 
+			cin>>str>>prec;
+			dic[str] = prec;
+		}
+		cin>>c;
+		while(c--) { //Averiguar valor total
+			cin>>str>>prec;
+			total+=dic[str]*prec; //Se multiplica el dato guardado en el producto por la cantidad
+		}
+		cout<<total<<"\n";
+	}
+	
 	return 0;
 }
