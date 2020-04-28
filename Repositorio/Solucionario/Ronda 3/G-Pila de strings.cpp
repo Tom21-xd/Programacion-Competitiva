8b13aa0a4c72959287f697201ef21523375ddd85
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-28 18:20:23 -0500

    Update G-Pila de strings.cpp

--- Repositorio/Solucionario/Ronda 3/G-Pila de strings.cpp
+++ Repositorio/Solucionario/Ronda 3/G-Pila de strings.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
-int main(){
-	fast;
+int main(int argc, char *argv[])
+{
+	int t,n,c,may,pos;
+	string entrada;
+	stack<string> pal;
+	cin>>n;
+	while(n--){
+		cin>>entrada;
+		if(entrada=="PUSH"){
+			cin>>entrada;
+			pal.push(entrada);
+		}else{
+			if(!pal.empty()){
+			cout<<pal.top()<<"\n";
+			pal.pop();
+			}else
+			cout<<"-\n";
+		}
+	}
 	return 0;
 }
