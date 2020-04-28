5f879d95046db64005293a199fd11a5caeba491a
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-28 18:40:36 -0500

    Update N-Cola de strings.cpp

--- Repositorio/Solucionario/Ronda 3/N-Cola de strings.cpp
+++ Repositorio/Solucionario/Ronda 3/N-Cola de strings.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
-int main(){
-	fast;
+
+int main()
+{
+	queue<string> f1;
+	string str;
+	int n;
+	cin>>n;
+	while(n--){
+		cin>>str;
+		if(str=="PUSH"){
+			cin>>str;
+			f1.push(str);
+		}else if(!f1.empty()){
+			cout<<f1.front()<<"\n";
+			f1.pop();
+		}else cout<<"-\n";
+	}
 	return 0;
 }
