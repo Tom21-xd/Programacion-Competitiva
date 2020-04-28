07294eb872f0744eaeaf8c7863956eacc965b9bf
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-28 18:30:36 -0500

    Update I-estacion de buses.cpp

--- Repositorio/Solucionario/Ronda 3/I-estacion de buses.cpp
+++ Repositorio/Solucionario/Ronda 3/I-estacion de buses.cpp
@@ -1,7 +1,52 @@
+//Solución 1 - Johan y Sierra
 #include <bits/stdc++.h>
 using namespace std;
 #define fast ios_base::sync_with_stdio(false);cin.tie(0);
 int main(){
-	fast;
+  int t,a,b,c;  
+  cin>>t;
+  queue <int> cola;
+  while(t--){
+    cin>>a;
+    while(a--){
+      cin>>b>>c;
+      if (c!=0){
+        cola.push(c);
+      } else {
+        if (!cola.empty()){
+          cout<<cola.front()<<"\n";
+          cola.pop();
+        }
+      }
+    } 
+  }
 	return 0;
 }
+
+//Solución 2 - SneiderQuintero (Fuerza Bruta)
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+	int t,b,op,input,i,j;
+	cin>>t;
+	while(t--){
+		i=j=0;
+		cin>>b;
+		int registro[b],salida[b];
+		while(b--){
+			cin>>op>>input;
+			if(op==1){
+				registro[i]=input;
+				i++;
+			}else{
+				salida[j]=registro[j];
+				j++;
+			}
+		}
+		for(i=0; i<j; i++){
+			cout<<salida[i]<<"\n";
+		}
+	}
+	return 0;
+} 
