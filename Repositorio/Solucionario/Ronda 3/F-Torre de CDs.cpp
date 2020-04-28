a5471882460331a7a9dd852822ad4d63452bdfb8
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-28 18:17:38 -0500

    Update F-Torre de CDs.cpp

--- Repositorio/Solucionario/Ronda 3/F-Torre de CDs.cpp
+++ Repositorio/Solucionario/Ronda 3/F-Torre de CDs.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
-int main(){
-	fast;
+int main(int argc, char *argv[])
+{
+	int t,b,input;
+	cin>>t;
+	while(t--){
+		stack<int>pila;
+		cin>>b;
+		while(b--){
+			cin>>input;
+			if(input>0)
+				pila.push(input);
+			else if(!pila.empty()){ //Si no esta vacia, mostramos y eliminamos.
+				cout<<pila.top()<<"\n";
+				pila.pop();
+			}
+		}
+	}
 	return 0;
 }
