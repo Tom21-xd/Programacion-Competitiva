6bddd0810c866a069dc8270a189c238ab8f4e3da
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-28 18:38:27 -0500

    Update M-Glosario.cpp

--- Repositorio/Solucionario/Ronda 3/M-Glosario.cpp
+++ Repositorio/Solucionario/Ronda 3/M-Glosario.cpp
@@ -1,7 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
-int main(){
-	fast;
+
+int main()
+{
+	set<string> f1;
+	int n, nP;
+	string str;
+	cin>>n;
+	while(n--){
+		f1.clear();
+		cin>>nP;
+		for(int i=0; i<nP; i++){
+			cin>>str;
+			int pos=str.size()-1;
+			//Elimino el último simbolo, si es que lo hay
+			if(str[pos]==',' || str[pos]=='.' || str[pos]=='?' || str[pos]=='!')
+				str.erase(str.begin()+pos); //Aqui se elimina
+			f1.insert(str);
+		}
+		cin>>fin;
+		//Muestro datos
+		for(string out : f1)
+			cout<<out<<"\n";
+		cout<<"-\n";
+	}
 	return 0;
 }
