e03df912d984c44efb99e9bab3862a5ef4c36c00
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-28 18:03:12 -0500

    Update C-Diccionario.cpp

--- Repositorio/Solucionario/Ronda 3/C-Diccionario.cpp
+++ Repositorio/Solucionario/Ronda 3/C-Diccionario.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
-int main(){
-	fast;
+
+int main()
+{
+	int n;
+	set<string> dic;
+	string str;
+	cin>>n;
+	while(n--){
+		dic.clear(); //Se vacía
+		cin>>f;
+		while(f--){
+			cin>>str;
+			dic.insert(str);
+		}
+		for(string out: dic)  //Muestra
+			cout<<out<<"\n";
+		cout<<"-\n";
+	}
 	return 0;
 }
