c6a4ac1f406c7e142bad605ec3babee232e22d60
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-28 18:33:48 -0500

    Update J-Hola mundo 2.0.cpp

--- Repositorio/Solucionario/Ronda 3/J-Hola mundo 2.0.cpp
+++ Repositorio/Solucionario/Ronda 3/J-Hola mundo 2.0.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
-int main(){
-	fast;
+int main()
+{
+	int n;
+	list<string> inp;
+	string str;
+	cin>>n;
+	while(n--){
+		cin>>str;
+		if(count(inp.begin(),inp.end(),str)==0) //Con la función count determinó si hay nombre repetido
+			inp.emplace_back(str);
+		else{
+			inp.remove(str); //Remuevo el nombre 
+			inp.emplace_back(str); //Lo agregó al final
+		}
+	}
+	for(auto it : inp) 
+		cout<<"Hola " <<it<<"\n";
+
 	return 0;
 }
+//Créditos JuanJr, yo solo lo implemente en c++
