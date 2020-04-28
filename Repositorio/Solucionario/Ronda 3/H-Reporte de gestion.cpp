0b844bdd0b3b49d22b25b720c59e16e89ed20311
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-28 18:27:29 -0500

    Update H-Reporte de gestion.cpp

--- Repositorio/Solucionario/Ronda 3/H-Reporte de gestion.cpp
+++ Repositorio/Solucionario/Ronda 3/H-Reporte de gestion.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
-int main(){
-	fast;
+int main()
+{
+	int n, user, t;
+	map<string, int> note;
+	string str;
+	cin>>t;
+	while(t--){
+		note.clear();
+		cin>>n;
+		while(n--){
+			cin>>str>>user;
+			note[str]=user;
+		}
+		map<string,int>::iterator outI=note.begin(); //Se crea iterador de map
+							     //Se puede reemplazar por auto
+		cout<<outI->first<<" = "<<outI->second<<"\n"; //Como map, funciona con pair, toca mostrar dato->first, dato->second. 
+		auto outF=note.rbegin(); //Aqui use el auto 
+					 //map<string,int>::reverse_iterator outF;
+		cout<<outF->first<<" = "<<outF->second<<"\n";
+	}
 	return 0;
 }
