f20fa618cda7832b8c38535fb8d1bd8c537eec29
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-28 18:16:02 -0500

    Update E-Parentesis.cpp

--- Repositorio/Solucionario/Ronda 3/E-Parentesis.cpp
+++ Repositorio/Solucionario/Ronda 3/E-Parentesis.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
-int main(){
-	fast;
+int main()
+{
+	int n;
+	string str;
+	cin>>n;
+	while(n--){
+		stack<char> inp; //Se crea aqui, para que aparezca vacia en cada caso
+		cin>>str;
+		for(char c : str) 
+		{
+			if(c=='(') //Se agrega
+				inp.push(c);
+			else if(!inp.empty() && c!=inp.top()) //Si es ')', se verifica si no esta vacío o si el dato top es diferente a ')' 
+				inp.pop();
+			else inp.push(')'); //Se agrega el ')' 
+		} 
+		if(inp.empty()) //Si la pila esta vacia es correcto
+			cout<<"CORRECTO\n";
+		else cout<<"INCORRECTO\n";
+	}
 	return 0;
 }
