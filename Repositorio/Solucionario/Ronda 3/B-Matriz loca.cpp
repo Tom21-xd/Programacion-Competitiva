4e4cde6773c54230e106c78f8d17b39e79ed9487
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-04-28 18:00:31 -0500

    Update B-Matriz loca.cpp

--- Repositorio/Solucionario/Ronda 3/B-Matriz loca.cpp
+++ Repositorio/Solucionario/Ronda 3/B-Matriz loca.cpp
@@ -1,7 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
-int main(){
-	fast;
+
+int main()
+{
+	int n, f, c;
+	cin>>n;
+	while(n--){
+		cin>>f>>c;
+		int mtz[f][c] ;
+		for(int i=0; i<f; i++){
+			for(int j=0; j<c; j++){
+				cin>>mtz[i][j]; //Se pide el dato en pos i y j
+				if(i%2==0 ) mtz[i][j]++; //Verifica si es una fila par y le suma
+				if(j%2==0 ) mtz[i][j]+=2; //Verifica si es una columna par y le suma
+				if(i%2!=0 && j%2!=0) mtz[i][j]-=3; //Verifica si es una columna y una fila impar y le resta
+			}
+		}
+		for(int i=0; i<f; i++){ //Muestra la nueva matriz
+			for(int j=0; j<c-1; j++){
+				cout<<mtz[i][j]<<" ";
+			}
+			cout<<mtz[i][c-1]<<"\n";
+		}
+	}
 	return 0;
 }
