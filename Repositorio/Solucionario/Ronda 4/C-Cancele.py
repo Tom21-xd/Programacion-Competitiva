750c15496eb6ee96d81f9c60dce2bf1e76111ede
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-05-06 19:35:11 -0500

    Update C-Cancele.py

--- Repositorio/Solucionario/Ronda 4/C-Cancele.py
+++ Repositorio/Solucionario/Ronda 4/C-Cancele.py
@@ -1,10 +1,9 @@
-#Hora de  tu trabajo ;v
+#Johan y Sierra - time: 0.025s
 from sys import stdin, stdout
 n= int(input())
 while n:
   n-=1
   a, b = stdin.readline().split()
-  a=int(a)
-  b=int(b)
+  a, b =int(a), int(b)
   c=a+b
   print(c)
