d0e625dadb575a669cfb620f786dff62bc33ffa4
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-05-06 20:18:08 -0500

    Update I-¡Huracán!.cpp

--- Repositorio/Solucionario/Ronda 4/I-¡Huracán!.cpp
+++ Repositorio/Solucionario/Ronda 4/I-¡Huracán!.cpp
@@ -1,1 +1,15 @@
-// tu  trabajo ;V
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios_base::sync_with_stdio(false); 
+  cin.tie(0); 
+  set<pair<int, int>>s;
+  int a, b;
+  while(cin>>a>>b){
+  	s.insert(make_pair(a,b));
+  }
+  for(auto it: s)
+  	cout<<it.first<<" "<<it.second<<"\n";
+  return 0;
+} 
