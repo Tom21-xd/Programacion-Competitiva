9675e03be33924a225572baa5645fbae54c11df5
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-05-06 19:17:28 -0500

    Update A-Tarea interminable.py

--- Repositorio/Solucionario/Ronda 4/A-Tarea interminable.py
+++ Repositorio/Solucionario/Ronda 4/A-Tarea interminable.py
@@ -1,4 +1,23 @@
-#Hora de  tu trabajo ;v
+#Solución 1 - SneiderQuintero - time: 0.028s
+def main(): #Definir clase main
+    from sys import stdin, stdout #importar entradas rápidas 
+    n = int(stdin.readline())
+    while n:
+    	n-=1
+    	op = stdin.readline()
+    	a = int(stdin.readline())
+    	b = int(stdin.readline())
+    	if op=="SUMA\n":
+    		stdout.write(str(a+b))
+    	elif op=='RESTA\n':
+    		stdout.write(str(a-b))
+    	else:
+    		stdout.write(str(a*b))
+    	stdout.write('\n')
+if __name__ == "__main__":
+    main()
+
+#Solución 2 - Johan y Sierra - time: 
 from sys import stdin, stdout
 n=stdin.readline()
 n=int(n)
