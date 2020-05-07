d678be9a288692cb368e75727f9535aae72cfa53
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-05-06 19:31:48 -0500

    Update B-Tarea Interminable ll.py

--- Repositorio/Solucionario/Ronda 4/B-Tarea Interminable ll.py
+++ Repositorio/Solucionario/Ronda 4/B-Tarea Interminable ll.py
@@ -1,4 +1,30 @@
-#Hora de  tu trabajo ;v
+#Solución 1 - SneiderQuintero - time: 0.029s
+def main(): #Crear clase main
+    from sys import stdin, stdout #importar fast i/o
+    n = int(stdin.readline()) # N casos
+    while n:
+    	n-=1
+    	n2,op= stdin.readline().split()
+    	n2=int(n2) # N2 números a operar
+    	if op=='SUMA':
+    		ans=0
+    		while n2:
+    			n2-=1
+    			a=int(stdin.readline())
+    			ans+=a
+    		stdout.write(str(ans%1007))
+    	else: #MULTIPLICACION
+    		ans=1
+    		while n2:
+    			n2-=1
+    			a=int(stdin.readline())
+    			ans*=a
+    		stdout.write(str(ans%1007))
+    	stdout.write('\n')
+if __name__ == "__main__":
+    main()
+
+#Solución 2 - Johan y Sierra - time: 0.028s
 from sys import stdin, stdout
 n=stdin.readline()
 n=int(n)
@@ -38,4 +64,36 @@ while n:
         c=c%1007
       con+=1 
       print(c)
-      
+
+
+#Solución 3 - JuanJr - C++ - time: 0.004s
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t, n;
+    ll acum, m;
+    cin >> t;
+    string s;
+    while(t--){
+        cin >> n >> s;
+        if(s[0] == 'S'){
+            acum = 0;
+            while(n--){
+                cin >> m;
+		//Propiedad de la aritmética modular
+                acum = (acum + m % 1007ll) % 1007ll;
+            }
+        }else{
+            acum = 1;
+            while(n--){
+                cin >> m;
+                acum = (acum * m % 1007ll) % 1007ll;
+            }
+        }
+        cout << acum << "\n";
+    }
+    return 0;
+}
