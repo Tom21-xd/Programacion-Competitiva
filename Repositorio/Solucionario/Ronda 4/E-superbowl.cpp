2cf0c884afc941fa14fd8c93e1f89070808d308d
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-05-06 19:56:01 -0500

    Update E-superbowl.cpp

--- Repositorio/Solucionario/Ronda 4/E-superbowl.cpp
+++ Repositorio/Solucionario/Ronda 4/E-superbowl.cpp
@@ -1,1 +1,44 @@
-// tu  trabajo ;V
+#Solución 1 - SneiderQuintero 
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+  ios_base::sync_with_stdio(false); 
+  cin.tie(0); 
+  int n;
+  ll s, d, a, b;
+  cin>>n;
+  while(n--){
+  	cin>>s>>d;
+  	b=abs(s-d)/2;
+  	a=s-b;
+  	if(a+b==s && abs(a-b)==d)
+  	   cout<<max(a,b)<<" "<<min(a,b)<<"\n";
+  	else cout<<"impossible\n";
+  }
+	return 0;
+}
+
+#Solución 2 - JuanJr
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+  ios_base::sync_with_stdio(false); 
+  cin.tie(0); 
+  int n;
+  ll s, d, a, b;
+  cin>>n;
+  while(n--){
+  	cin>>s>>d;
+  	s-=d; //Verificar que la diferencia sea menor a la sumatoria 
+  	if(s>=0 && !(s & 1)){
+  		s>>=1; //Hayo b
+  		d+=s; //Hayo a
+  		cout<<max(s,d)<<" "<<min(s,d)<<"\n";
+  	}else cout<<"impossible\n";
+  }
+	return 0;
+} 
