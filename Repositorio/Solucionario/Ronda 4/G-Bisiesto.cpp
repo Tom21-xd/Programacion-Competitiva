149b65c9ba02372d1bb2f583ebb9e3f85cac70bb
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-05-06 20:08:10 -0500

    Update G-Bisiesto.cpp

--- Repositorio/Solucionario/Ronda 4/G-Bisiesto.cpp
+++ Repositorio/Solucionario/Ronda 4/G-Bisiesto.cpp
@@ -1,4 +1,4 @@
-// tu  trabajo ;V
+//Solución 1 - Johan
 #include <bits/stdc++.h>
 using namespace std;
 #define fast ios_base::sync_with_stdio(false);cin.tie(0);
@@ -17,4 +17,22 @@ int main(){
 		cout<<res<<endl;
 	}
 	return 0;
+}
+
+//Solución 2 - SneiderQuintero
+#include <bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+int main()
+{
+  ios_base::sync_with_stdio(false); 
+  cin.tie(0); 
+  ull act, mod, pasd;
+  while(cin>>act>>mod>>pasd){
+  	if(act%mod==0) pasd--;
+  	else
+  	  while(act%mod!=0) act++;
+  	cout<<act-(mod*pasd)<<"\n";
+  }
+  return 0;
 }
