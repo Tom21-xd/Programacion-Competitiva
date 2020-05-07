872efe6e9fa9386a3ad68ba566f85544d4434dbd
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-05-06 20:01:50 -0500

    Update F-The 3n+1  problem.cpp

--- Repositorio/Solucionario/Ronda 4/F-The 3n+1  problem.cpp
+++ Repositorio/Solucionario/Ronda 4/F-The 3n+1  problem.cpp
@@ -1,1 +1,25 @@
-// tu  trabajo ;V
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios_base::sync_with_stdio(false); 
+  cin.tie(0); 
+  int i, j, mn, mx, c, ans;
+  while(cin>>i>>j){
+  	ans=0;
+  	mn = min(i, j);
+  	mx = max(i, j);
+  	for(int k = mn; k<=mx; k++){
+  		c=1;
+  		int ward=k;
+  		while(ward!=1){
+  			if(ward & 1) ward=(ward*3)+1;
+  			else ward/=2;
+  			c++;
+  		}
+  		ans=c>ans?c:ans;
+  	}
+  	cout<<i<<" "<<j<<" "<<ans<<"\n";
+  }
+	return 0;
+} 
