1835d0358bb0ce2913ee8f0dedfa9a693fcf5e4f
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-05-06 20:38:20 -0500

    Update M-Los 3 hermanos.cpp

--- Repositorio/Solucionario/Ronda 4/M-Los 3 hermanos.cpp
+++ Repositorio/Solucionario/Ronda 4/M-Los 3 hermanos.cpp
@@ -1,4 +1,4 @@
-// tu  trabajo ;V
+// Solución 1 - Joham
 #include <bits/stdc++.h>
 using namespace std;
 #define fast ios_base::sync_with_stdio(false);cin.tie(0);
@@ -20,5 +20,26 @@ int main(){
 		}
 		cout<<a<<" "<<b<<" "<<c<<endl;
 	}
+	return 0;
+}
+
+//Solución 2 - SneiderQuintero
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios_base::sync_with_stdio(false); 
+  cin.tie(0); 
+  int dulces, david, darla, daniel;
+  while(cin>>dulces){
+  	david=darla=daniel=dulces/3;
+  	if(dulces%3==1)
+  		darla++;
+  	else{
+  		darla++;
+  		david++;
+  	}
+  	cout<<darla<<" "<<david<<" "<<daniel<<"\n";
+  }
 	return 0;
 }
