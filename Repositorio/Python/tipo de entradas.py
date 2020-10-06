47f1cef87b262ecfaec62ea35c4e08cea00ee151
SneiderQuintero <62130443+SneiderQuintero@users.noreply.github.com>
2020-10-06 14:42:40 -0500

    Update tipo de entradas.py

--- Repositorio/Python/tipo de entradas.py
+++ Repositorio/Python/tipo de entradas.py
@@ -1,18 +1,20 @@
-#Leer varios datos en una sola linea
-
-data = input().split(' ')
+#Leer varios datos en una sola line
+data = input().split(Caracter separador)
 
 #N casos de prueba
-
 while n:
 	n-=1
 	#Instrucciones
 
 # Casos de prueba hasta EOF	
-
-Try:
-	# lo que se lee ;v
-except:
-	break
-	#proceder a codear
+while True:
+	try:
+		# lo que se lee ;v
+	except:
+		break
+		#proceder a codear
+		
+#Leer todas las lineas
+from sys import stdin, stdout
+buff = stdin.readlines()
 		
